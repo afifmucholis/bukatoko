9b408a39e3e0d1c5cd52f455e2da1e6d333d283a
afif <afif@restock.id>
2021-04-12 11:43:53 +0700

    Add: frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Baju Koko',
+            category: 'Category',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Koko',
+            rating: 4,
+            numReviews: 10,
+            description: 'Bajunya koko woyo'
+        },
+        {
+            _id: '2',
+            name: 'Baju Kiki',
+            category: 'Category',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Koko',
+            rating: 4,
+            numReviews: 10,
+            description: 'Bajunya koko woyo'
+        },
+        {
+            _id: '3',
+            name: 'Baju Kiku',
+            category: 'Category',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Koko',
+            rating: 4,
+            numReviews: 10,
+            description: 'Bajunya koko woyo'
+        },
+        {
+            _id: '4',
+            name: 'Baju Kuku',
+            category: 'Category',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Koko',
+            rating: 4,
+            numReviews: 10,
+            description: 'Bajunya koko woyo'
+        },
+    ],
+}
+
+export default data;
