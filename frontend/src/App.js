8361f8cc025f70e39203c69961f48ccb6ddc9c9d
afif <afif@restock.id>
2021-04-13 08:26:34 +0700

    Rename app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="index.html">amazona</a>
-        </div>
-        <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/" component={HomeScreen} exact></Route>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">BUKATOKO</a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
