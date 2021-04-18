cfce1c9d64f361fdcd45e641250d7093168bb748
afif <afif@restock.id>
2021-04-18 12:39:52 +0700

    Add: ssignin action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreeen from './screens/CartScreeen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -9,6 +10,13 @@ import SigninScreen from './screens/SigninScreen';
 function App() {
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -18,12 +26,29 @@ function App() {
           </div>
           <div>
             <Link to="/cart">Cart
-            {
+              {
                 cartItems.length > 0 && (
                   <span className="badge"> {cartItems.length}</span>
                 )
-              }</Link>
-            <Link to="/signin">Sign In</Link>
+              }
+            </Link>
+            {
+              userInfo
+                ? (
+                  <div className="dropdown">
+                    <Link to="#">
+                      {userInfo.name} <i className="fa fa-caret-down"></i> {''}
+                    </Link>
+                    <ul className="dropdown-content">
+                      <Link to="/signout" onClick={signoutHandler}>Signout</Link>
+                    </ul>
+                  </div>
+                )
+                : (
+                  <Link to="/signin">Sign In</Link>
+                )
+            }
+
           </div>
         </header>
         <main>
